dfed6425b32f6f71a452e196127bdbe0fd597d91
redwananuar <redwananuar@gmail.com>
2025-07-22 14:07:53 +0800

    rebuild 3

--- src/components/Scheduler.tsx
+++ src/components/Scheduler.tsx
@@ -4,9 +4,9 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import '@fullcalendar/common/main.css';
-import '@fullcalendar/daygrid/main.css';
-import '@fullcalendar/timegrid/main.css';
+import '@fullcalendar/common/index.css';
+import '@fullcalendar/daygrid/index.css';
+import '@fullcalendar/timegrid/index.css';
 import { X } from 'lucide-react';
 
 // Color map for job status
